1eda79d340b4b2429c62e247c67e7aaa41f945f2
Shubhranil Basak <144095577+Shubhranil-Basak@users.noreply.github.com>
2024-03-13 01:07:11 +0530

    Update generalCalc.py

--- calculator/generalCalc.py
+++ calculator/generalCalc.py
@@ -1,9 +1,37 @@
+"""
+# Module: generalCalc
+
+## Description: It can perform basic aritmetics
+
+## Supported functions:
+    1. Sin
+    2. Cos
+    3. Tan
+    4. Cot
+    5. Sec
+    6. Cosec
+    7. Cot
+    8. Log
+    9. ln
+    10. fac -> factorial
+    11. Exp/e^
+    12. abs
+    13. sqrt
+
+## Constants:
+    1. pi
+    2. e
+
+## Input Format:
+    1. Arguments of a function must be inside paranthesis, example: 'sin x' should be written as 'sin(x)'.
+    2. Just the expression in x or constants, no need to write 'y = '.
+"""
+
+
 import numpy as np
 
 def main():
-    """
-    It can do basic arithmetic and can find values of different functions.
-    """
+    
     while True:
         a = input("Enter your numbers and functions to calculate: ")
         if a.lower() == 'end':
@@ -21,7 +49,7 @@ def main():
             a = a.replace('fac','math.factorial')
             a = a.replace('log', 'np.log10')
             a = a.replace('ln', 'np.log')
-            a = a.replace('mod', 'np.abs')
+            a = a.replace('abs', 'np.abs')
             a = a.replace('e^', 'np.exp')
             a = a.replace('e', 'np.e')
 
